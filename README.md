23883bea955c457bcca2505900da83a565c7a6c1
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2024-11-04 22:51:49 +0000

    add documentation and fixed median function

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is my submission for my internship at jetbrains
+
+## How to run?
+Run the command:
+`python controller.py "randomNum.py"`
--- controller.py
+++ controller.py
@@ -2,10 +2,19 @@ import subprocess
 import sys
 
 def write(process, str):
+    """
+    Writes to the process the desired string.
+    Handles adding newline at the end of the string and
+    flushing the stdout so the string is definitely sent
+    """
     process.stdin.write(f'{str}\n')
     process.stdin.flush()
 
 def readline(process):
+    """
+    Reads a line from stdin from the process
+    Removes unnecessary whitespace and the newline character
+    """
     return process.stdout.readline().strip()
 
 def average(numList):
@@ -16,20 +25,38 @@ def average(numList):
     return sum / len(numList)
 
 def median(sortedNumList):
+    """
+    Returns the median of the list
+    Precondition: the list is already sorted
+    """
     medianPosition = (len(sortedNumList) + 1) / 2
     fractionalPart = medianPosition - int(medianPosition)
+    medianIndex = int(medianPosition) - 1
     if (fractionalPart == 0):
-        return sortedNumList[medianPosition]
+        return sortedNumList[medianIndex]
     
-    return (sortedNumList[int(medianPosition)] + sortedNumList[int(medianPosition)]) / 2
+    return (sortedNumList[medianIndex] + sortedNumList[medianIndex + 1]) / 2
+
+def getRandomList(process):
+    """
+    Gets a list of 100 random elements from the process
+    """
+    randNumList = []
+    for i in range(100):
+        write(process, "GetRandom")
+        randNum = readline(process)
+        randNumList.append(int(randNum))
+    return randNumList
 
 def main():
+    # Gets program name from program arguments
     if len(sys.argv) <= 1:
         print("No program passed in as argument")
         return
     
     program = sys.argv[1]
 
+    # creates the subprocess
     randomNumProcess = subprocess.Popen(
         ['python', program],
         stdin=subprocess.PIPE, 
@@ -38,17 +65,16 @@ def main():
         text=True
     )
 
+
+    # Performs the "Hi function call"
     write(randomNumProcess, "Hi")
     hiResponse = readline(randomNumProcess)
     if hiResponse != "Hi":
         print("Did not return correct \"Hi\" response")
 
-    randNumList = []
-    for i in range(100):
-        write(randomNumProcess, "GetRandom")
-        randNum = readline(randomNumProcess)
-        randNumList.append(int(randNum))
+    randNumList = getRandomList(randomNumProcess)
     
+    # shutsdown the subprocess
     write(randomNumProcess, "Shutdown")
     returnCode = randomNumProcess.wait()
 
